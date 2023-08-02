75adc6b3ed0752ca065f2e36f9bda2736497ec6f
huijia-v <2921523918@qq.com>
2023-08-02 21:17:17 +0800

    初始化rpc轮子项目

--- huijia-demo/src/test/java/com/huijia/NettyTest.java
+++ huijia-demo/src/test/java/com/huijia/NettyTest.java
@@ -1,10 +1,17 @@
 package com.huijia;
 
+import com.huijia.netty.AppClient;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.CompositeByteBuf;
 import io.netty.buffer.Unpooled;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @program:
@@ -39,6 +46,25 @@ public class NettyTest {
         ByteBuf byteBuf = Unpooled.wrappedBuffer(buf, buy2);
 
 
+    }
+
+    @Test
+    public void testMessage() throws IOException {
+
+        ByteBuf message = Unpooled.buffer();
+        message.writeBytes("huijia".getBytes(StandardCharsets.UTF_8));
+        message.writeByte(1);
+        message.writeShort(125);
+        message.writeInt(256);
+
+        // 用对象流转换为字节数据
+        AppClient appClient = new AppClient();
+        byte[] buffer = new byte[1024];
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ObjectOutput oos = new ObjectOutputStream(outputStream);
+        oos.writeObject(appClient);
+        byte[] bytes = outputStream.toByteArray();
+        message.writeBytes(bytes);
     }
 
 }
